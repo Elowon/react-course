66df187d48856dd7b009043668e74a5df655fa4e
Elo <105211140+Elowon@users.noreply.github.com>
2025-09-30 14:38:22 +0300

    Update OrdersGrid.jsx

--- ecommerce-project/src/pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/pages/orders/OrdersGrid.jsx
@@ -70,7 +70,7 @@ export function OrdersGrid({ orders, loadCart }) {
                     </div>
 
                     <div className="product-actions">
-                      <a href="/tracking">
+                      <a href={`/tracking/${order.id}/${orderProduct.product.id}`}>
                         <button className="track-package-button button-secondary">
                           Track package
                         </button>
