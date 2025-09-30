08216046e42d0b98e01e9278aa326abe1e89e79b
Elo <105211140+Elowon@users.noreply.github.com>
2025-09-30 14:42:34 +0300

    Update NotFoundPage.jsx

--- ecommerce-project/src/pages/NotFoundPage.jsx
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,14 +1,15 @@
 import { HeaderPage } from "../components/HeaderPage";
 import "./NotFoundPage.css";
 
-export function NotFoundPage() {
+export function NotFoundPage({ cart }) {
   return (
     <>
       <title>404 Page Not Found</title>
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
-      <HeaderPage />
+      <HeaderPage cart={cart} />
       <div className="not-found-message">Page not found</div>
     </>
   );
 }
+
